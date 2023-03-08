c5e6a968422c6dcfc655231c12d28b33bcbb3703
Stead08 <62312581+Stead08@users.noreply.github.com>
2023-03-08 10:00:15 +0900

    Create str_find.rs

---
+++ str_find.rs
@@ -0,0 +1,15 @@
+//findメソッドを使って文字列の検索
+fn main() {
+    let s = "隣の客はよく柿食う客だ";
+
+    //'柿'を検索
+    match s.find('柿') {
+        Some(i) => println!("柿={}B", i),
+        None => println!("柿はなし"),
+    };
+    //'バナナ'を検索
+    match s.find("バナナ") {
+        Some(i) => println!("バナナ={}B", i),
+        None => println!("バナナはなし"),
+    };
+}
